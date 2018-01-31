50eccfa399ff17e421d47c844a744f8f28e35649
Alex Norton <alexnortn@gmail.com>
2018-01-30 21:37:37 -0500

    added underline for navigation

---
+++ dist/assets/pdo.js
@@ -0,0 +1,9 @@
+// Custom JS elements for PDO site
+// Alex Norton 2018
+
+$(function() {
+	console.log('hello');
+	$("#collection-title").text("Things");
+	// how to create a random grid system
+	// a simple algorithm
+});
